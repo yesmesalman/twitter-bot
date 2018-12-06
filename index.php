f0898256061ed3ad45879fccc425e6710a3d8847
Salman Saleem <salmanmemon569@yahoo.com>
2018-12-06 22:23:09 +0500

    Twitter Bot

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+date_default_timezone_set("Asia/Karachi");
+$log_file = 'file.txt';
+if(!file_exists($log_file)){
+	$fp = fopen($log_file, "w");
+}
+$fp = fopen($log_file, 'w');
+fwrite($fp, date('Y-m-d H:i:s'));
+fclose($fp);
+require_once 'src/twitter.class.php';
+    $consumerKey = '##';
+    $consumerSecret = '##';
+    $accessToken = '##';
+    $accessTokenSecret = '##';
+    $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+    $results = $twitter->search(['q' => 'follow everyone who likes this','count'=>'1', 'result_type'=>'recent']);
+    echo "<script>console.log(".json_encode($results, True).")</script>";
+?>
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=ibm855">
+	<title>TWEETS</title>
+	
+	<style type="text/css">
+		body{
+			font-family: verdana;
+			padding: 12px;
+		}
+		a{
+			color:#3f51b5;
+			font-size: 12px;
+		}
+		table{
+			margin: 0 auto;
+			text-align: center;
+			border: 1px solid #ccc;
+    		padding: 10px;
+		}
+		table tr td{
+			border-top: 1px solid #ccc;
+		}
+	</style>
+</head>
+<body>
+<center><h3>hey! this is result of my Job</h3></center>
+<?php 
+	if(count($results)){
+	$count=1;
+	echo "<table>
+		<tr>
+			<td><strong>..</strong></td>
+			<td><strong>visit tweet</strong></td>
+			<td><strong>tweet</strong></td>
+			<td><strong>user</strong></td>
+			<td><strong>created at</strong></td>
+		</tr>
+	";
+		foreach ($results as $status){
+		 $twitter->favThis($status->id);
+		// $twitter->rtThis($status->id);
+?>
+		<tr>
+			<td>
+				<span><?php echo $count++; ?></span>
+			</td>
+			<td>
+				<a href="https://twitter.com/andreasfaille/status/<?php echo $status->id ?>" target="_blank">Visit tweet</a>
+			</td>
+			<td>
+				<a href="https://twitter.com/andreasfaille/status/<?php echo $status->id ?>" target="_blank"><?php echo $status->text; ?></a>
+			</td>
+			<td>
+				<a href="https://twitter.com/<?php echo $status->user->screen_name ?>"><?php echo $status->user->name ?></a>
+			</td>
+			<td>
+				<span><?php echo $status->created_at; ?></span>
+			</td>
+		</tr>
+<?php 
+		}
+	echo "</table>";
+	}
+?>
+</body>
+</html>
